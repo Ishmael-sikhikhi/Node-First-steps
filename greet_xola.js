73589e3304e3e4db9b9ab80a8aec84de57c52838
ishmal-sikhikhi <i.sikhikhi@gmail.com>
2021-07-20 13:36:10 +0200

    updade modules and .gitignore

--- greet_xola.js
+++ greet_xola.js
@@ -1,18 +1,23 @@
+"use strict"
 const greet = require('./greet');
 // figlet starts
 var figlet = require('figlet')
+const chalk = require('chalk');
 
+
+var myData
 figlet(greet('Xola'), function (err, data){
+    myData =  data;
     if(err) {
         console.log('Something went wrong...');
         console.dir(err);
         return;
     }
-    console.log(data)
+    //import the greet module that is in the current folder
+    const styledMessage = chalk.bgGreen.black(data);
+    console.log(styledMessage)
 })
 
-const chalk = require('chalk');
-//import the greet module that is in the current folder
 
-const styledMessage = chalk.bgGreen.black(figlet);
-console.log(styledMessage)
+
+
